814ab54e6e9d9888038d125d478fadcb145c4088
Panos61 <k702943@students.kastoria.teiwm.gr>
2021-01-08 23:02:59 +0200

    Added Welcome page

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Welcome } from './Welcome';
+export { default as Dashboard } from './Dashboard';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
